33364fe70428cc205a93b77f75d40c6999ad3ab3
linhareshian <hian.linhares@rethink.dev>
2024-08-24 21:05:34 -0300

    finalizando o projeto

--- Src/Javascript/script.js
+++ Src/Javascript/script.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-    $('.mobile_btn-default').on('click', function(){
-        $('.mobile_menu').toggleClass('active')
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    if (index >= slides.length) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = slides.length - 1;
+    } else {
+        currentSlide = index;
+    }
+    
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === currentSlide) {
+            slide.classList.add('active');
+        }
     });
-});
+}
+
+function moveSlide(step) {
+    showSlide(currentSlide + step);
+}
+
+// Exibe o slide inicial
+showSlide(currentSlide);
